832458bcdf28e23e1662a23159b6a024b1740159
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-06-22 08:24:18 +1200

    Update calcultor.js
    Adding the JS file

--- calcultor.js
+++ calcultor.js
@@ -1,1 +1,17 @@
-Adding the JS  file 
+// Adding the JS  file 
+
+var equation = "";
+var screen = document.getElementById("screen");
+function equationBuilder(number) {
+  equation += number;
+  screen.innerHTML = equation;
+}
+function runEquals() {
+  equals = eval(equation);
+  screen.innerHTML = equals;
+}
+function runClear() {
+  equation = 0;
+  screen.innerHTML = " ";
+}
+
